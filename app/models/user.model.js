7f841578c87a58468db4eca0de4c5372db27b6cb
liorAbuhav <lior290999@gmail.com>
2022-06-18 19:10:07 +0300

    users crud

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,18 +1,16 @@
 module.exports = mongoose => {
     var schema = mongoose.Schema(
       {
-        id: Number,
+        id: String,
         first_name: String,
         last_name: String,
         birthday: Date,
         marital_status: String
-      },
-      { timestamps: true }
+      }
     );
   
     schema.method("toJSON", function() {
       const { __v, _id, ...object } = this.toObject();
-      object.id = _id;
       return object;
     });
   
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -6,23 +6,17 @@ module.exports = app => {
     // Create a new User
     router.post("/", users.create);
   
-    // // Retrieve all Tutorials
-    // router.get("/", tutorials.findAll);
+    // Retrieve all Users
+    router.get("/", users.findAll);
   
-    // // Retrieve all published Tutorials
-    // router.get("/published", tutorials.findAllPublished);
+    // Retrieve a single User with id
+    router.get("/:id", users.findOne);
   
-    // // Retrieve a single Tutorial with id
-    // router.get("/:id", tutorials.findOne);
+    // Update a User with id
+    router.put("/:id", users.update);
   
-    // // Update a Tutorial with id
-    // router.put("/:id", tutorials.update);
-  
-    // // Delete a Tutorial with id
-    // router.delete("/:id", tutorials.delete);
-  
-    // // Create a new Tutorial
-    // router.delete("/", tutorials.deleteAll);
+    // Delete a User with id
+    router.delete("/:id", users.delete);
   
     app.use("/api/users", router);
   };
