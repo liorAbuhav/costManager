51ad704ac0bef8fea6ae4f61ba31ae4800d059f1
Niv Hamisha <78423434+NivHamisha@users.noreply.github.com>
2022-06-26 19:28:37 +0300

    removed comments

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 const express = require("express");
-// const cors = require("cors");
 const app = express();
 
-// var corsOptions = {
-//   origin: "http://localhost:8081"
-// };
-
-// app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
 app.use(express.json());  /* bodyParser.json() is deprecated */
-
-// // parse requests of content-type - application/x-www-form-urlencoded
-// app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
 
 const db = require("./app/models");
 db.mongoose
